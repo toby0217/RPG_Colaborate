551f841373fdf026370c6167991af0225bc19e5d
bubble <116156247+ididw@users.noreply.github.com>
2026-06-16 13:05:51 +0800

    Define Item class with constructors and methods

--- Item.cpp
+++ Item.cpp
@@ -1,6 +1,40 @@
-#include "Item.h"
+#ifndef ITEM_H
+#define ITEM_H
+
+#include <string>
 
 namespace RPG_Colaborate
 {
-    
+    class Item
+    {
+    private:
+        std::string name;
+        std::string type;
+        int effectValue;
+        int quantity;
+
+    public:
+        // 建構子
+        Item(std::string name, std::string type, int effectValue, int quantity);
+        ~Item();
+
+        // Getters
+        std::string getName() const;
+        std::string getType() const;
+        int getEffectValue() const;
+        int getQuantity() const;
+
+        // Setters
+        void setName(const std::string& newName);
+        void setType(const std::string& newType);
+        void setEffectValue(int newValue);
+        void setQuantity(int newQuantity);
+
+        // 使用是大概這樣.....吧
+        void use();
+        void showInfo() const;
+        bool isAvailable() const;
+    };
 }
+
+#endif
