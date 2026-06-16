63bdb597288d339a4b5b3d439ca9b79d9f83253d
jinrisong017 <jinrisong17@gmail.com>
2026-06-16 22:47:20 +0800

    update monster

--- Monster.h
+++ Monster.h
@@ -1,19 +1,44 @@
 #ifndef MONSTER_H
 #define MONSTER_H
-
+#include<string>
 namespace RPG_Colaborate
 {
+    class Player;
+    
     class Monster
     {
     private:
-        /* data */
-    public:
-        Monster(/* args */);
-        ~Monster();
+        std::string name;
+        int hp;
+        int attackPower;
+        int rewardGold;
 
-        void takeDamage(int damage) {
-            std::cout << "[測試用] 怪物受到了 " << damage << " 點傷害！\n";
-        }
+    public:
+        //建構子
+        Monster();
+        Monster(const std::string& name,int hp,int attackPower,int rewardGold);
+
+
+        //getter
+        std::string getName()const;
+        int getHp()const;
+        int getAttackPower()const;
+        int getRewardGold()const;
+
+
+        //setter
+        void setName(const std::string& name);
+        void setHp(int hp);
+        void setAttackPower(int attackPower);
+        void setRewardGold(int rewardGold);
+
+
+        //function
+        void attack(Player& player)const;
+        void takeDamage(int damage);
+        bool isAlive()const;
+        void showInfo()const;
+
     };
 }
 
