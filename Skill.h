4e5aec31529ec14b01b47f9e343f04b8d93a5ed0
bubble <116156247+ididw@users.noreply.github.com>
2026-06-16 14:09:00 +0800

    Update Skill.h

--- Skill.h
+++ Skill.h
@@ -1,15 +1,36 @@
 #ifndef SKILL_H
 #define SKILL_H
 
+#include <string>
+
 namespace RPG_Colaborate
 {
     class Skill
     {
     private:
-        /* data */
+        std::string name;
+        //技能的傷害應該會跟角色攻擊力有關 那這邊的damage是不是可以改成技能倍率之類的
+        int damage;
+        int mpCost;
+
     public:
-        Skill(/* args */);
+        // 建構子
+        Skill(std::string name, int damage, int mpCost);
         ~Skill();
+
+        // Getters
+        std::string getName() const;
+        int getDamage() const;
+        int getMpCost() const;
+
+        // Setters
+        void setName(const std::string& newName);
+        void setDamage(int newDamage);
+        void setMpCost(int newMpCost);
+
+        // 用技能 嗯大概這樣
+        void use();
+        void showInfo() const;
     };
 }
 
